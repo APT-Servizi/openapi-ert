c31bbb264a1aefdb8ee075bf25ca6a7f6e22fbb1
Stefano Vena <2668723-wetfire2k@users.noreply.gitlab.com>
2021-10-09 10:30:03 +0200

    Fix Licenza

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-SCRIPT_DIR="$( cd "$(dirname "$( dirname "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
-
+dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(dirname "$dir")"
 redoc-cli bundle --options.noAutoAuth=true -o $SCRIPT_DIR/docs/index.html $SCRIPT_DIR/v1/api-spec.json 
