e184d6c6c9fc5cfd5791734054defd43e71b987d
Stefano Vena <2668723-wetfire2k@users.noreply.gitlab.com>
2021-10-09 10:49:54 +0200

    Aggiornamento descrizione

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SCRIPT_DIR="$(dirname "$dir")"
-redoc-cli bundle --options.noAutoAuth=true -o $SCRIPT_DIR/docs/index.html $SCRIPT_DIR/v1/api-spec.json 
+redoc-cli bundle --options.noAutoAuth=true -o $SCRIPT_DIR/docs/index.html $SCRIPT_DIR/v1/api-spec.yaml 
